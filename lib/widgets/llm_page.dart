4670e216b9d8014c1b7928b4d3134f74bdcb77fc
sixtick_sub3 <business4dyd@gmail.com>
2024-12-27 10:07:48 +0900

    llm https modify

--- lib/widgets/llm_page.dart
+++ lib/widgets/llm_page.dart
@@ -5,6 +5,10 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';  // JSON 처리 라이브러리
 // notifier , provider, consumer
 
+
+const String defaultRole = '사용자가 입력한 숫자 중에 제일 큰 숫자를 말해보세요. 상냥하고 해맑고 귀여운 꼬마 말투로 대답해줘';
+const String defaultQuery = '10, 20, 30, 40, 100, 60, 70, 80';
+
 class LLMPage extends ConsumerWidget{
 
   @override
@@ -12,8 +16,8 @@ class LLMPage extends ConsumerWidget{
     final TextEditingController _controller = TextEditingController();
     final TextEditingController _controller_answer = TextEditingController();
     final TextEditingController _controller_query = TextEditingController();
-    _controller.text = '사용자가 입력한 숫자 중에 제일 큰 숫자를 말해보세요.';
-    _controller_query.text = '10, 20, 30, 40, 100, 60, 70, 80';
+    _controller.text = defaultRole;
+    _controller_query.text = defaultQuery;
 
     return Scaffold(
       body: Column(
@@ -66,7 +70,19 @@ class LLMPage extends ConsumerWidget{
           ),
           Padding(
             padding: const EdgeInsets.all(8.0),
-            child: ElevatedButton(onPressed: (){}, child: Text('전송')),
+            child: FutureBuilder(
+              future: null,
+              // future: query(_controller.text, _controller_query.text),
+              builder: (context, snapshot) {
+                return ElevatedButton(onPressed: (){
+                  query(_controller.text, _controller_query.text)
+                    .then((onValue){
+                      debugPrint(onValue);
+                      _controller_answer.text = onValue;
+                    });
+                }, child: Text('전송'));
+              }
+            ),
           ),
         ],
       ),
@@ -79,23 +95,25 @@ class LLMPage extends ConsumerWidget{
 Future<String> query(String role, String query) async{
   String res = '';
   // var url = Uri.https(address, 'llm');
-  var url = Uri.https('sixtick.duckdns.org:19821', 
+  var url = Uri.https('sixtick.duckdns.org', 
     'llm',
     {
       'role' : role,
       'query' : query
     });
-  var response = await http.get(url);
+  // var response = await http.get(url);
   // debugPrint('response.statusCode : ${response.body}');
   // List<dynamic> jsonList = jsonDecode(response.body);
   // debugPrint('jsonList ${jsonList[10]['turn']}');
   
   try {
-      // GET 요청 보내기
+      // GET 요청 보내기 cp949.cp949.decode(response.bodyBytes)
       var response = await http.get(url);
-
+      debugPrint('query try ${response.statusCode}');
       if (response.statusCode == 200) {
         // 요청 성공 시
+        // res = jsonDecode(response.body)['answer'];
+        res = jsonDecode(utf8.decode(response.bodyBytes))['answer'];
         
       } else {
         // 요청 실패 시
@@ -103,9 +121,9 @@ Future<String> query(String role, String query) async{
       }
     } catch (e) {
       // 네트워크 오류 처리
-      
+      debugPrint('query catch ${e}');
     }
-  return '';
+  return res;
 }
 
 //http://sixtick.duckdns.org:19821/llm?role=%EC%82%AC%EC%9A%A9%EC%9E%90%EA%B0%80%20%EC%A0%9C%EC%8B%9C%ED%95%9C%20%EC%88%AB%EC%9E%90%EB%93%A4%20%EC%A4%91%EC%97%90%20%EC%A0%9C%EC%9D%BC%20%ED%81%B0%20%EC%88%AB%EC%9E%90%EB%A5%BC%20%EB%A7%90%ED%95%B4%EC%A4%98&query=1,2,3,4,10,6,7,8,9
