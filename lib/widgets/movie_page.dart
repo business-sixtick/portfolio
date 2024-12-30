5583ec6d640af7986af0ea97daf15fdea73f7262
sixtick_sub3 <business4dyd@gmail.com>
2024-12-30 18:20:55 +0900

    .

---
+++ lib/widgets/movie_page.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:http/http.dart' as http;
+
+// https://api.themoviedb.org/3/movie/550?api_key=7279d5c699c77014f9045c749a1badc0
