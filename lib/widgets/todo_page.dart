b6ca38095ab2c38374f443caf3ef58f030b26341
sixtick_sub3 <business4dyd@gmail.com>
2025-01-02 17:33:09 +0900

    sembast 작성중

---
+++ lib/widgets/todo_page.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sembast/sembast.dart';
+import 'package:sembast/sembast_io.dart';  // flutter pub add sembast
+import 'package:path/path.dart';
+
+// flutter pub add path_provider
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class TodoDb {
+  TodoDb._internal();
+  static final TodoDb _singleton = TodoDb._internal();
+  factory TodoDb(){ // 싱글톤, 계속 생성해도 같은 객체임
+    return _singleton;
+  }
+  DatabaseFactory dbFactory = databaseFactoryIo;
+  final store = intMapStoreFactory.store('todos');
+  
+  Database? _database;
+  Future<Database> get database async {
+    if(_database == null){
+      await _openDb().then((db){
+        _database = db;
+      });
+    }
+    return _database!;
+  }
+
+  Future _openDb() async {
+    final docsPath = await getApplicationDocumentsDirectory();
+    final dbPath = join(docsPath.path, 'todos.db');
+    final db = await dbFactory.openDatabase(dbPath);
+    return db;
+  }
+}
+
+
+class Todo{
+  late int id;
+  late String name;
+  late String description;
+  late String completeBy;
+  late int priority; // 우선 순위, 프라이오러티, 프라이오리
+
+  Todo(this.name, this.description, this.completeBy, this.priority);
+
+  Map<String, dynamic> toMap(){
+    return{
+      'name' : name,
+      'description' : description,
+      'completeBy' : completeBy,
+      'priority' : priority,
+    };
+  }
+
+  static Todo fromMap(Map<String, dynamic> map){
+    return Todo(map['name'], map['description'], map['completeBy'], map['priority']);
+  }
+}
